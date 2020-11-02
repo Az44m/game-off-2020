68a7fe3d156eb6e8efb931839444c02db8e76713
Tamas Miskolczi <miskolczitamas17@gmail.com>
2020-11-03 00:12:13 +0100

    Read words

--- Entities/HelloWorld.cs
+++ Entities/HelloWorld.cs
@@ -1,3 +1,4 @@
+using GameOff2020.Entities.Services;
 using Godot;
 
 namespace GameOff2020.Entities
@@ -11,7 +12,11 @@ namespace GameOff2020.Entities
         // Called when the node enters the scene tree for the first time.
         public override void _Ready()
         {
-            GD.Print("Hello World");
+            var wordService = new WordService();
+            GD.Print($"Hello World - {string.Join(",", wordService.GetRandomLetters(10))}");
+            GD.Print($"Not valid: {wordService.IsValidWord("gfdsgdfs")}");
+            GD.Print($"Valid: {wordService.IsValidWord("Apple")}");
+            GD.Print($"Score: {wordService.CalculateScore("Apple")}");
         }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
---
+++ Entities/Services/WordService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Godot;
+using NReco.Csv;
+
+namespace GameOff2020.Entities.Services
+{
+    public class WordService
+    {
+        private readonly Assembly _executingAssembly;
+        private readonly Random _random;
+        private readonly List<string> _words = new List<string>();
+
+        private readonly Dictionary<char, int> _letters = new Dictionary<char, int>
+        {
+            {'A', 1},
+            {'E', 1},
+            {'I', 1},
+            {'O', 1},
+            {'N', 1},
+            {'R', 1},
+            {'T', 1},
+            {'L', 1},
+            {'S', 1},
+            {'U', 1},
+            {'D', 2},
+            {'G', 2},
+            {'B', 3},
+            {'C', 3},
+            {'M', 3},
+            {'P', 3},
+            {'F', 4},
+            {'H', 4},
+            {'W', 4},
+            {'V', 4},
+            {'Y', 4},
+            {'K', 5},
+            {'J', 8},
+            {'X', 8},
+            {'Q', 10},
+            {'Z', 10}
+        };
+
+        public WordService()
+        {
+            _executingAssembly = Assembly.GetExecutingAssembly();
+            _random = new Random();
+            ReadWords();
+        }
+
+        private void ReadWords()
+        {
+            var wordsStream = _executingAssembly.GetManifestResourceStream("GameOff2020.Resources.words.txt");
+            using var streamReader = new StreamReader(wordsStream!);
+
+            var csvReader = new CsvReader(streamReader);
+            while (csvReader.Read())
+            {
+                var word = csvReader[0];
+                if (!word.Contains("'"))
+                {
+                    if (!_words.Contains(word))
+                        _words.Add(word.ToUpper());
+                    else
+                        GD.Print($"Vótmá: {word}");
+                }
+            }
+        }
+
+        public int CalculateScore(string word) => word.ToUpper().Sum(c => _letters[c]);
+
+        public bool IsValidWord(string word) => _words.Contains(word.ToUpper());
+
+        public char[] GetRandomLetters(int amount)
+        {
+            return _letters.Keys.OrderBy(x => _random.Next()).Take(amount).ToArray();
+        }
+    }
+}

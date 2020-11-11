b1b4de148cd59f2f696b36019c69c2b746cd857e
Tamas Miskolczi <miskolczitamas17@gmail.com>
2020-11-11 21:15:53 +0100

    Update word dictionary

--- Entities/Services/WordService.cs
+++ Entities/Services/WordService.cs
@@ -11,93 +11,41 @@ namespace GameOff2020.Entities.Services
     public class WordService : Node
     {
         private Assembly _executingAssembly;
+        private char[] _currentLetters;
         private readonly Random _random = new Random();
         private readonly List<string> _words = new List<string>();
 
         private readonly Dictionary<char, int> _letters = new Dictionary<char, int>
         {
-            {'A', 1},
-            {'E', 1},
-            {'I', 1},
-            {'O', 1},
-            {'U', 1},
-            {'N', 1},
-            {'R', 1},
-            {'T', 1},
-            {'L', 1},
-            {'S', 1},
-            {'D', 2},
-            {'G', 2},
-            {'B', 3},
-            {'C', 3},
-            {'M', 3},
-            {'P', 3},
-            {'F', 4},
-            {'H', 4},
-            {'W', 4},
-            {'V', 4},
-            {'Y', 4},
-            {'K', 5},
-            {'J', 8},
-            {'X', 8},
-            {'Q', 10},
-            {'Z', 10}
+            {'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}, {'N', 1}, {'R', 1}, {'T', 1}, {'L', 1}, {'S', 1}, {'D', 2}, {'G', 2}, {'B', 3},
+            {'C', 3}, {'M', 3}, {'P', 3}, {'F', 4}, {'H', 4}, {'W', 4}, {'V', 4}, {'Y', 4}, {'K', 5}, {'J', 8}, {'X', 8}, {'Q', 10}, {'Z', 10}
         };
 
-        private readonly Dictionary<char, int> _vowels = new Dictionary<char, int>
-        {
-            {'A', 1},
-            {'E', 1},
-            {'I', 1},
-            {'O', 1},
-            {'U', 1}
-        };
+        private readonly Dictionary<char, int> _vowels = new Dictionary<char, int> {{'A', 1}, {'E', 1}, {'I', 1}, {'O', 1}, {'U', 1}};
 
         private readonly Dictionary<char, int> _consonants = new Dictionary<char, int>
         {
-            {'N', 1},
-            {'R', 1},
-            {'T', 1},
-            {'L', 1},
-            {'S', 1},
-            {'D', 2},
-            {'G', 2},
-            {'B', 3},
-            {'C', 3},
-            {'M', 3},
-            {'P', 3},
-            {'F', 4},
-            {'H', 4},
-            {'W', 4},
-            {'V', 4},
-            {'Y', 4},
-            {'K', 5},
-            {'J', 8},
-            {'X', 8},
-            {'Q', 10},
-            {'Z', 10}
+            {'N', 1}, {'R', 1}, {'T', 1}, {'L', 1}, {'S', 1}, {'D', 2}, {'G', 2}, {'B', 3}, {'C', 3}, {'M', 3}, {'P', 3},
+            {'F', 4}, {'H', 4}, {'W', 4}, {'V', 4}, {'Y', 4}, {'K', 5}, {'J', 8}, {'X', 8}, {'Q', 10}, {'Z', 10}
         };
-
-        private char[] _currentLetters;
 
         public override void _Ready()
         {
             _executingAssembly = Assembly.GetExecutingAssembly();
-            ReadWords();
+            LoadWords();
         }
 
-        private void ReadWords()
+        private void LoadWords()
         {
+            if (_words.Any())
+                return;
+
             var wordsStream = _executingAssembly.GetManifestResourceStream("GameOff2020.Resources.words.txt");
             using var streamReader = new StreamReader(wordsStream!);
 
             var csvReader = new CsvReader(streamReader);
             while (csvReader.Read())
-            {
-                var word = csvReader[0];
-                if (!word.Contains("'"))
-                    _words.Add(word.ToUpper());
-            }
+                _words.Add(csvReader[0]);
         }
 
         public int CalculateScore(string word) => word.ToUpper().Sum(c => _letters[c]);
@@ -106,7 +54,7 @@ namespace GameOff2020.Entities.Services
 
         public char[] GetRandomLetters(int amount)
         {
-            var consonants = _consonants.Keys.OrderBy(x => _random.Next()).Take(amount-5).ToArray();
+            var consonants = _consonants.Keys.OrderBy(x => _random.Next()).Take(amount - 5).ToArray();
             _currentLetters = consonants.Concat(_vowels.Keys).OrderBy(x => _random.Next()).ToArray();
             return _currentLetters;
         }

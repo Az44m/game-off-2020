b985d14f8f840da8ca7ab8ac30660a71632477e2
Tamas Miskolczi <miskolczitamas17@gmail.com>
2020-11-20 22:41:05 +0100

    Play alarm sound when spy is exposed

--- Entities/Guards/GuardBase.cs
+++ Entities/Guards/GuardBase.cs
@@ -6,9 +6,14 @@ namespace GameOff2020.Entities.Guards
 {
     public abstract class GuardBase : AnimatedSprite
     {
+        private AudioStreamPlayer2D _exposedSpyAudioPlayer;
         protected SignalService SignalService { get; private set; }
 
-        public override void _Ready() => SignalService = GetNode<SignalService>("/root/SignalService");
+        public override void _Ready()
+        {
+            SignalService = GetNode<SignalService>("/root/SignalService");
+            _exposedSpyAudioPlayer = GetNode<AudioStreamPlayer2D>("ExposedSpyAudioPlayer");
+        }
 
         protected async void OnSpyExposed(SpyBase spy)
         {
@@ -17,6 +22,7 @@ namespace GameOff2020.Entities.Guards
 
             Frame = 1;
             spy.RunAway();
+            _exposedSpyAudioPlayer.Play();
             await ToSignal(GetTree().CreateTimer(1, false), "timeout");
             Frame = 0;
         }
